f78a1de08dd4fa928bb1a6337107bdfea5d37184
Andrea N. Cardona <andreancardona@gmail.com>
2021-03-06 08:39:52 -0700

    update(navigation): added styles to navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,19 +1,20 @@
+import React from 'react';
 import profilePlaceholder from '../images/profilePlaceholder.svg';
 import '../styles/Navigation.css';
 
 const Navigation = () => {
   return (
-  <div className="banner" role="banner">
     <div className="nav-container">
-      <div className="header" tabIndex="0" >Welcome to JobHob</div>
-        <img className="user-profile--image" alt="profile placeholder" tabIndex="0"  src={profilePlaceholder} />
-        <div className="user-info">
-        <div className="user-profile--name" tabIndex="0">Andrea Cardona</div>
-        <div className="user-profile--role" tabIndex="0">Admin</div>
-      </div>
-    </div>
-  </div>
-);
+      <div className="nav-title" tabIndex="0" >Welcome to JobHob</div>
+        <div className="user-profile">
+          <img className="user-profile--image" alt="profile placeholder" tabIndex="0"  src={profilePlaceholder} />
+          <div className="user-profile--info">
+            <div className="user-profile--name" tabIndex="0">Andrea Cardona</div>
+            <div className="user-profile--role" tabIndex="0">Admin</div>
+          </div>
+        </div>
+     </div>
+  );
 }
 
 export default Navigation;
