2dc625c9ff881e27a6b022fa0bbde8d7b63df0aa
Andrea N. Cardona <andreancardona@gmail.com>
2021-03-10 18:48:55 -0700

    refactor(jobForm): small refactor

--- src/components/JobForm.js
+++ src/components/JobForm.js
@@ -13,44 +13,36 @@ const JobForm = (props) => {
 
   const subHeader = props.adding ? 'Fill out the information for your new job listing.' : 'Edit the information for your job listing.';
 
-  // const jobFormLabels = [
-  //   {
-  //     label: props.labels.title,
-  //     subLabel: props.subLabels.title,
-  //     input: props.job.title,
-  //   }
-  // ]
-  
   return (
     <Fragment>
       <div className="form-headers">
-      <div className="form-heading">
-        {header}
-      </div>
-      <div className="form-sub-heading">
-        {subHeader}
-      </div>
+        <div className="form-heading">
+          {header}
+        </div>
+        <div className="form-sub-heading">
+          {subHeader}
+        </div>
       </div>
       <form className="job-form">
         <label className="label">{props.labels.title}</label>
-          <div className="sub-label">{props.subLabels.title}</div>
+        <div className="sub-label">{props.subLabels.title}</div>
           <input className="input" type="text" name="title" value={props.job.title} onChange={handleInputChange} />
         <label className="label">{props.labels.location}</label>
-          <div className="sub-label">{props.subLabels.location}</div>
+        <div className="sub-label">{props.subLabels.location}</div>
           <input className="input" type="text" name="location" value={props.job.location} onChange={handleInputChange} />
         <label className="label">{props.labels.sponsorship}</label>
-          <div className="sub-label">{props.subLabels.sponsorship}</div>    
-            <select className="select-input" name="sponsorship" onChange={handleInputChange} value={props.job.sponsorship}>
-              <option value="Sponsor">Sponsor</option>
-              <option value="Free">Free</option>
-            </select>
+        <div className="sub-label">{props.subLabels.sponsorship}</div>    
+          <select className="select-input" name="sponsorship" onChange={handleInputChange} value={props.job.sponsorship}>
+            <option value="Sponsor">Sponsor</option>
+            <option value="Free">Free</option>
+          </select>
         <label className="label">{props.labels.status}</label>
-          <div className="sub-label">{props.subLabels.status}</div> 
-            <select className="select-input" onChange={handleInputChange} name="status" value={props.job.status}>
-              <option value="Open">Open</option>
-              <option value="Paused">Paused</option>
-              <option value="Closed">Closed</option>
-            </select>
+        <div className="sub-label">{props.subLabels.status}</div> 
+          <select className="select-input" onChange={handleInputChange} name="status" value={props.job.status}>
+            <option value="Open">Open</option>
+            <option value="Paused">Paused</option>
+            <option value="Closed">Closed</option>
+          </select>
       </form>
     </Fragment>  
   )
